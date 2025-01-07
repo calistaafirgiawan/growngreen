7a6e453c0f36cf9da72381ee3b5d568a63abc721
calistaafirgiawan <calista1016@gmail.com>
2025-01-07 09:21:50 +0700

    Grow And Green

--- landing.php
+++ landing.php
@@ -102,5 +102,28 @@ $ctaLink = "growandgreen.php";
         <p>Hijaukan Bumi, Wujudkan Masa Depan 🌱</p>
         <a href="<?php echo $ctaLink; ?>" class="cta-button"><?php echo $ctaText; ?></a>
     </div>
+    <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA1p6u2nOvucRMSQwzXL_CPzJLw2KIbzIk",
+    authDomain: "growngreen-9647d.firebaseapp.com",
+    projectId: "growngreen-9647d",
+    storageBucket: "growngreen-9647d.firebasestorage.app",
+    messagingSenderId: "190470948404",
+    appId: "1:190470948404:web:f683d475f87c10379bec61",
+    measurementId: "G-3B7D6D9RYK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 </body>
 </html>
